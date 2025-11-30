1930b73d973160308be816de51f2d7781cca8b89
Jake Adamson <jakemadamson2k14@gmail.com>
2025-11-30 18:34:20 +0000

    auto-commit for 94c5644f-6307-4bfb-9017-41d939d912fd

--- frontend/app/(tabs)/_layout.tsx
+++ frontend/app/(tabs)/_layout.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Tabs } from 'expo-router';
 import { Ionicons } from '@expo/vector-icons';
+import { View } from 'react-native';
 import { useLanguageMode } from '../../contexts/LanguageModeContext';
 import { useTheme } from '../../contexts/ThemeContext';
 
