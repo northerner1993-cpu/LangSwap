d6306c074505915195027ae67ca09f15a7847244
Jake Adamson <jakemadamson2k14@gmail.com>
2025-12-01 12:32:24 +0000

    auto-commit for fb01933d-638f-4ac9-b895-f9b4ec857209

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -2,21 +2,26 @@ import { Stack } from 'expo-router';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { ThemeProvider } from '../contexts/ThemeContext';
 import { LanguageModeProvider } from '../contexts/LanguageModeContext';
+import { UILanguageProvider } from '../contexts/UILanguageContext';
 
 export default function RootLayout() {
   return (
-    <ThemeProvider>
-      <LanguageModeProvider>
-        <SafeAreaProvider>
-          <Stack screenOptions={{ headerShown: false }}>
-            <Stack.Screen name="index" options={{ headerShown: false }} />
-            <Stack.Screen name="language-selection" options={{ headerShown: false }} />
-            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-            <Stack.Screen name="lesson/[id]" options={{ headerShown: false }} />
-            <Stack.Screen name="songs-catalogue" options={{ headerShown: false }} />
-          </Stack>
-        </SafeAreaProvider>
-      </LanguageModeProvider>
-    </ThemeProvider>
+    <UILanguageProvider>
+      <ThemeProvider>
+        <LanguageModeProvider>
+          <SafeAreaProvider>
+            <Stack screenOptions={{ headerShown: false }}>
+              <Stack.Screen name="index" options={{ headerShown: false }} />
+              <Stack.Screen name="language-selection" options={{ headerShown: false }} />
+              <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+              <Stack.Screen name="lesson/[id]" options={{ headerShown: false }} />
+              <Stack.Screen name="songs-catalogue" options={{ headerShown: false }} />
+              <Stack.Screen name="auth/login" options={{ headerShown: false }} />
+              <Stack.Screen name="auth/register" options={{ headerShown: false }} />
+            </Stack>
+          </SafeAreaProvider>
+        </LanguageModeProvider>
+      </ThemeProvider>
+    </UILanguageProvider>
   );
 }
