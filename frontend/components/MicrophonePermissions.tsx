f2ab58580c21964d2cfd994e1aad02ed509326c0
Jake Adamson <jakemadamson2k14@gmail.com>
2025-12-01 16:12:27 +0000

    auto-commit for 0a438a08-2e07-45fb-8c01-d2229639c3ff

--- frontend/components/MicrophonePermissions.tsx
+++ frontend/components/MicrophonePermissions.tsx
@@ -73,7 +73,7 @@ export default function MicrophonePermissions({ colors }: MicrophonePermissionsP
     if (Platform.OS === 'ios') {
       Linking.openURL('app-settings:');
     } else {
-      Linking.openSettings();
+      Linking.openURL('app-settings:');
     }
   };
 
